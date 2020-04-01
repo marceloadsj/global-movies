1112982a973d281bbcf5d7e962ae07c1324eed96
Marcelo Junior <marceloadsj1@gmail.com>
2020-04-01 20:43:52 +0200

    improvement: moving some classes to a better place

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import TheHeader from "features/app/organisms/TheHeader";
 
 export default function App() {
   return (
-    <main className="min-h-screen bg-gray-900 p-5">
+    <>
+      <main className="px-5 pt-5 pb-16">
       <TheHeader />
     </main>
+    </>
   );
 }
