e4e9d030777fe3efd369038390d78705b78a2001
Marcelo Junior <marceloadsj1@gmail.com>
2020-04-01 19:50:29 +0200

    feature: created a simple Logo component

---
+++ src/features/app/atoms/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Icon from "components/atoms/Icon";
+
+export default function Logo() {
+  return (
+    <div className="uppercase font-black text-white text-2xl">
+      Global M<Icon name="film" className="mb-1" />
+      vies
+    </div>
+  );
+}
