b45b0326cf8de20794044cb335664fce83b45cfd
Marcelo Junior <marceloadsj1@gmail.com>
2020-04-01 20:44:25 +0200

    feature: created the footer unique component

---
+++ src/features/app/organisms/TheFooter.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Icon from "components/Icon";
+
+export default function TheFooter() {
+  return (
+    <footer className="absolute bottom-0 w-full h-16 flex items-center justify-between px-5 text-sm border-t border-gray-800">
+      <div className="text-gray-600">
+        Created at <Icon name="home" /> with <Icon name="heart" /> and{" "}
+        <Icon name="coffee" />
+      </div>
+
+      <a
+        href="https://github.com/marceloadsj/global-movies"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-gray-500"
+      >
+        Marcelo Silva - Github
+      </a>
+    </footer>
+  );
+}
