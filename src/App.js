c658452102296c4a14dfca456e1a8dde87767cb4
Marcelo Junior <marceloadsj1@gmail.com>
2020-04-04 16:43:04 +0200

    feature: added config action load on App comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
+import getConfiguration from "features/app/actions/getConfiguration";
+import AppLoader from "features/app/organisms/AppLoader";
+import AppError from "features/app/organisms/AppError";
 import TheHeader from "features/app/organisms/TheHeader";
 import TheFooter from "features/app/organisms/TheFooter";
+import HomePage from "features/home/pages/HomePage";
 
 export default function App() {
+  // dispatch to get the configurations object, as it's needed all over the system
+  const dispatch = useDispatch();
+
+  const configuration = useSelector((state) => state?.configuration);
+
+  useEffect(() => {
+    if (!configuration) {
+      dispatch(getConfiguration());
+    }
+  }, [configuration, dispatch]);
+
   return (
     <>
-      <main className="p-5 flex-1">
-        <TheHeader />
+      <main className="flex-1 flex flex-col">
+        {(!configuration || configuration?.loading) && <AppLoader />}
+
+        {configuration?.error && <AppError>{configuration.message}</AppError>}
+
+        {configuration?.success && (
+          <>
+            <TheHeader />
+
+            <Switch>
+              <Route path="/" exact>
+                <HomePage />
+              </Route>
+            </Switch>
+          </>
+        )}
       </main>
 
       <TheFooter />
