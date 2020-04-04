731df5238ff8a1bda66cdc368c6ec95ee2686a8f
Marcelo Junior <marceloadsj1@gmail.com>
2020-04-04 18:39:15 +0200

    Updates

---
+++ precache-manifest.19efd6322d258095c40ddcabce75f99b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3a50faeb5a47fa774cf3925bce80a7",
+    "url": "/global-movies/index.html"
+  },
+  {
+    "revision": "a414f08da218450e1f09",
+    "url": "/global-movies/static/css/main.c510e220.chunk.css"
+  },
+  {
+    "revision": "3e77f744f779a9cfdebf",
+    "url": "/global-movies/static/js/2.b0cfe55d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/global-movies/static/js/2.b0cfe55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a414f08da218450e1f09",
+    "url": "/global-movies/static/js/main.693faaa6.chunk.js"
+  },
+  {
+    "revision": "c7be74a472f701b77ac2",
+    "url": "/global-movies/static/js/runtime-main.8c4f0693.js"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/global-movies/static/media/tmdb-logo.56fd61ee.svg"
+  }
+]);
