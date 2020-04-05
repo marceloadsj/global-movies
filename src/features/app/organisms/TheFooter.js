aab35c5d716a037d95511b04fd328b73c078e416
Marcelo Junior <marceloadsj1@gmail.com>
2020-04-05 15:08:04 +0200

    improvement: add tmdb logo to footer

--- src/features/app/organisms/TheFooter.js
+++ src/features/app/organisms/TheFooter.js
@@ -4,21 +4,35 @@ import Icon from "components/Icon";
 
 export default function TheFooter() {
   return (
-    <footer className="w-full h-16 flex items-center justify-between px-5 text-sm border-t border-gray-800">
+    <footer className="w-full h-24 flex flex-col sm:flex-row items-center justify-between py-3 px-5 text-sm border-t border-gray-800">
       <div className="text-gray-600 flex items-center">
         Created at <Icon name="home" className="mx-1" /> with{" "}
         <Icon name="heart" className="mx-1" /> and{" "}
         <Icon name="coffee" className="ml-1" />
       </div>
 
-      <a
-        href="https://github.com/marceloadsj/global-movies"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-gray-500"
-      >
-        Marcelo da Silva<span className="hidden sm:inline"> - Github</span>
-      </a>
+      <div className="text-center sm:text-right">
+        <a
+          href="https://github.com/marceloadsj/global-movies"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500"
+        >
+          Marcelo da Silva<span className="hidden sm:inline"> - Github</span>
+        </a>
+
+        <div className="text-white mt-2">
+          Powered by{" "}
+          <a
+            href="https://www.themoviedb.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-orange-500"
+          >
+            themoviedb.org
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
