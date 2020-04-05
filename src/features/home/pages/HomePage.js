e7d0b176f82ae2219b6dd84a229d5c3cd5a0ee2e
Marcelo Junior <marceloadsj1@gmail.com>
2020-04-05 17:44:19 +0200

    feature: added the movie comps to movie feat

--- src/features/home/pages/HomePage.js
+++ src/features/home/pages/HomePage.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 import GenresNav from "features/genres/organisms/GenresNav";
-import FeaturedMovie from "../organisms/FeaturedMovie";
-import MoviesList from "../organisms/MoviesList";
+import MoviesList from "features/movies/organisms/MoviesList";
+import MovieDetails from "features/movies/organisms/MovieDetails";
 import MovieSectionTemplate from "../templates/MovieSectionTemplate";
 
 export default function HomePage() {
   return (
     <>
-      <FeaturedMovie />
+      <MovieDetails
+        movieId={process.env.REACT_APP_FEATURED_MOVIE_ID}
+        isFeatured
+      />
 
       <MovieSectionTemplate>
         <GenresNav />
